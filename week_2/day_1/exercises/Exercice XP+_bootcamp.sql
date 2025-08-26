d0898b515e3a3d9715cc3a38160b39994acb1ab1
chaimae <chaimaehassari1@email.com>
2025-08-26 15:16:13 +0100

    Exrecises XP Gold

--- week_2/day_1/exercises/Exercice XP+_bootcamp.sql
+++ week_2/day_1/exercises/Exercice XP+_bootcamp.sql
@@ -25,3 +25,17 @@ SELECT first_name, last_name FROM students WHERE first_name ILIKE '%a';
 SELECT first_name, last_name FROM students WHERE first_name LIKE '%a_';
 SELECT first_name, last_name FROM students WHERE id IN (1, 3);
 SELECT * FROM students WHERE birth_date >= '2000-01-01';
+
+
+-- ==================Exrecises XP Gold=================
+SELECT first_name, last_name ,birth_date FROM students 
+ORDER BY last_name ASC;
+LIMIT 4;
+
+SELECT first_name, last_name ,birth_date FROM students
+ORDER BY birth_date DESC;
+LIMIT 1;
+
+SELECT first_name, last_name ,birth_date FROM students
+OFFSET 2
+LIMIT 3;
