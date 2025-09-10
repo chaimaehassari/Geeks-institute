68b579783d7bfcb9e711196bdd29262c4d366e14
chaimae <chaimaehassari1@email.com>
2025-09-10 09:41:36 +0100

    Add initial implementations for "Daily challengeNot Bad" and "Daily challengeStars" exercises

---
+++ week_3/day_1/Daily challengeStars.js
@@ -0,0 +1,14 @@
+
+let stars="";
+for(let i=0;i<7;i++){
+    stars+="*";
+    console.log(stars);
+}
+for(let j=0;j<7-i-1;j++){
+    let row="";
+    for(let k=0;k<j+1;k++){
+        row+="*";
+    }
+
+        console.log("row"); 
+    }   
