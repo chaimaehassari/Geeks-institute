01e88fd3cf840d93cdcd0f97bdac8d02e6042a1a
chaimae <chaimaehassari1@email.com>
2025-09-18 19:34:10 +0100

    challenge

--- week_3/day_3/daily_challenges/challenges.js
+++ week_3/day_3/daily_challenges/challenges.js
@@ -1,5 +1,35 @@
-// ====== Daily Challenge 1
+const planets = [
+  {name: "Mercure", color: "gray", moons: 0},
+  {name: "Venus", color: "yellow", moons: 0},
+  {name: "Terre", color: "blue", moons: 1},
+  {name: "Mars", color: "red", moons: 2},
+  {name: "Jupiter", color: "orange", moons: 4},
+  {name: "Saturne", color: "gold", moons: 3},
+  {name: "Uranus", color: "lightblue", moons: 2},
+  {name: "Neptune", color: "darkblue", moons: 1}
+];
 
-// ====== Daily Challenge 2
+let section = document.querySelector('.listPlanets');
 
-// ====== Daily Challenge 3
+for (let plant of planets) {
+    let divPlanet = document.createElement('div');
+    divPlanet.className = 'planet';
+    divPlanet.style.backgroundColor = plant.color;
+    divPlanet.textContent = plant.name;
+
+    for (let i = 0; i < plant.moons; i++) {
+        let divMoon = document.createElement('div');
+        divMoon.classList.add('moon');
+
+        let angle = (i / plant.moons) * 2 * Math.PI;
+
+        let radius = 60; 
+
+        divMoon.style.left = `${50 + radius * Math.cos(angle)}px`;
+        divMoon.style.top = `${50 + radius * Math.sin(angle)}px`;
+
+        divPlanet.appendChild(divMoon);
+    }
+
+    section.appendChild(divPlanet);
+}
